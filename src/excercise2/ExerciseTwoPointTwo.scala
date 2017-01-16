d6a5b68f88bde9349c0d7b9e03d29e2e85bfb6a5
Siddharth Khialani <siddharthkhialani@gmail.com>
2017-01-16 13:44:48 -0500

    exercise2.2

---
+++ src/excercise2/ExerciseTwoPointTwo.scala
@@ -0,0 +1,17 @@
+package excercise2
+
+object ExerciseTwoPointTwo {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean) : Boolean = {
+    def loop(n: Int): Boolean = {
+      if (as.length <= n) true
+      else if (ordered(as(n-1), as(n))) loop(n+1)
+      else false
+    }
+    if (as == null) true
+    else loop(1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(isSorted(Array(1, 2, 3), (a: Int, b: Int) => a < b))
+  }
+}
