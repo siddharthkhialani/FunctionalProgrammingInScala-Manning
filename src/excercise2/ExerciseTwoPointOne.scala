bd735406b332b0d9fcd547769d83dee7bed92e99
Siddharth Khialani <siddharthkhialani@gmail.com>
2017-01-15 23:03:18 -0500

    Exercise 2.1 first pass

---
+++ src/excercise2/ExerciseTwoPointOne.scala
@@ -0,0 +1,19 @@
+package excercise2
+
+object ExerciseTwoPointOne {
+  def fib(n: Int) = {
+    def loop(b: Int, acc: Int, n: Int): Int = {
+      if(n <= 0) acc else loop(acc, b + acc, n - 1)
+    }
+
+    if(n == 1) {
+      0
+    } else if(n == 2) {
+      1
+    } else loop(1, 1, n - 2)
+  }
+
+  def main(args: Array[String]) : Unit = {
+    println(fib(5))
+  }
+}
